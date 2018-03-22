98848f2213d76cd80c888e8f835f273faa41b4c1
jasminbi <jasmin.bissig@gmail.com>
2018-03-22 20:35:00 +0100

    Rename templates

--- challenges/urls.py
+++ challenges/urls.py
@@ -4,9 +4,9 @@ from django.conf.urls import url
 from . import views
 
 
-#assign a view called challenges_list to ^$ URL (only empty string will match) > 'http://127.0.0.1:8000/' is not a part of the URL
+#assign a view called landing to ^$ URL (only empty string will match) > 'http://127.0.0.1:8000/' is not a part of the URL
 urlpatterns = [
     url(r'^$',views.landing, name='landing'),
-    url(r'^$',views.challenges_list, name='challenges_list'),
+    url(r'^$',views.active_challenge, name='active_challenge'),
     url(r'^$',views.old_challenges, name='old_challenges'),
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -4,18 +4,20 @@ from .models import Challenge
 from django.template.loader import get_template
 
 # Create your views here.
-def landing(request):
-    return render(request, 'challenges/landing.html', {'landing': landing})
 
-def challenges_list(request):
-    challenges = Challenge.objects.filter(challenge_start__lte=timezone.now())
-    return render(request, 'challenges/challenges_list.html',
-    {'challenges': challenges})
+def landing(request):
+    return render(request, 'challenges/landing.html',{'landing': landing})
+
+def active_challenge(request):
+    active_challenge = Challenge.objects.filter(challenge_active=True)
+    return render(request, 'challenges/active_challenge.html',
+    {'active_challenge': active_challenge})
 
 def old_challenges(request):
-    return render(request, 'challenges/old_challenges.html', {})
+    old_challenge = Challenge.objects.filter(challenge_start__lte=timezone.now(), challenge_active=False)
+    return render(request, 'challenges/old_challenges.html', {'old_challenge' : old_challenge})
     #
     # old_challenges = Challenge.objects.filter(
     # challenge_stop__lte=timezone.now()).order_by('challenge_start')
-    # return render(request, 'challenges/challenges_list.html',
+    # return render(request, 'challenges/landing.html',
     # {'old_challenges': old_challenges})
