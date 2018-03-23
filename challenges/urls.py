8ae7645118852db6219c23e98d1608b195e6908e
jasminbi <jasmin.bissig@gmail.com>
2018-03-23 07:44:28 +0100

    Combined old and active challenge in one view (challenges)

--- challenges/urls.py
+++ challenges/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 #assign a view called landing to ^$ URL (only empty string will match) > 'http://127.0.0.1:8000/' is not a part of the URL
 urlpatterns = [
-    url(r'^$',views.landing, name='landing'),
-    url(r'^$',views.active_challenge, name='active_challenge'),
-    url(r'^$',views.old_challenges, name='old_challenges'),
+    url(r'^$',views.challenges, name='challenges'),
+    # url(r'^$',views.challenges, name='challenges'),
+    # url(r'^$',views.old_challenges, name='old_challenges'),
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -5,19 +5,14 @@ from django.template.loader import get_template
 
 # Create your views here.
 
-def landing(request):
-    return render(request, 'challenges/landing.html',{'landing': landing})
+def challenges (request):
+    active_challenges = Challenge.objects.filter(challenge_active=True)
+    old_challenges = Challenge.objects.filter(challenge_start__lte=timezone.now(), challenge_active=False)
+    return render(request, 'challenges/landing.html',{'challenges': challenges})
 
-def active_challenge(request):
-    active_challenge = Challenge.objects.filter(challenge_active=True)
-    return render(request, 'challenges/active_challenge.html',
-    {'active_challenge': active_challenge})
+# def old_challenges (request):
+#     old_challenges = Challenge.objects.filter(challenge_start__lte=timezone.now(), challenge_active=False)
+#     return render(request, 'challenges/landing.html',{'old_challenges': old_challenges})
 
-def old_challenges(request):
-    old_challenge = Challenge.objects.filter(challenge_start__lte=timezone.now(), challenge_active=False)
-    return render(request, 'challenges/old_challenges.html', {'old_challenge' : old_challenge})
-    #
-    # old_challenges = Challenge.objects.filter(
-    # challenge_stop__lte=timezone.now()).order_by('challenge_start')
-    # return render(request, 'challenges/landing.html',
-    # {'old_challenges': old_challenges})
+
+# old_challenges = Challenge.objects.filter(challenge_start__lte=timezone.now(), challenge_active=False)
