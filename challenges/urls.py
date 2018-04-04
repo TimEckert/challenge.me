ec438fccad2dc5165ea7ff8677bcff7c868ab64f
jasminbi <jasmin.bissig@gmail.com>
2018-04-04 20:59:29 +0200

    Resolved merge conflicts

--- challenges/urls.py
+++ challenges/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 #assign a view called landing to ^$ URL (only empty string will match) > 'http://127.0.0.1:8000/' is not a part of the URL
 urlpatterns = [
-    url(r'^$',views.challenges, name='challenges'),
+    url(r'^$', views.challenges, name='challenges'),
     # url(r'^$',views.challenges, name='challenges'),
     # url(r'^$',views.old_challenges, name='old_challenges'),
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -5,14 +5,13 @@ from django.template.loader import get_template
 
 # Create your views here.
 
-def challenges (request):
-    active_challenges = Challenge.objects.filter(challenge_active=True)
-    old_challenges = Challenge.objects.filter(challenge_start__lte=timezone.now(), challenge_active=False)
-    return render(request, 'challenges/landing.html',{'challenges': challenges})
-
-# def old_challenges (request):
+# def challenges(request):
+#     active_challenges = Challenge.objects.filter(challenge_active=True)
 #     old_challenges = Challenge.objects.filter(challenge_start__lte=timezone.now(), challenge_active=False)
-#     return render(request, 'challenges/landing.html',{'old_challenges': old_challenges})
+#     return render(request, 'challenges/landing.html', {'challenges': challenges})
 
-
-# old_challenges = Challenge.objects.filter(challenge_start__lte=timezone.now(), challenge_active=False)
+def challenges(request):
+    challenges = Challenge.objects.all()
+    return render(request, 'challenges/landing.html', {'challenges' : challenges})
+    some_challenges = Challenge.objects.filter(title__contains='Console')
+    return render(request, 'challenges/landing.html', {'some_challenges' : some_challenges})
